77af8d3019e35be5a545939bacc4f647b6b23052
Théophile Cantelobre <theophilec@gmail.com>
2026-07-07 21:11:54 +0200

    Simplify

--- datasette_syntaqlite/__init__.py
+++ datasette_syntaqlite/__init__.py
@@ -1,31 +1,8 @@
-import mimetypes
-import os
-
 import syntaqlite as syntaqlite
-from datasette import Response, hookimpl
+from datasette import hookimpl
 from markupsafe import escape
 
 from .routes import lint_view
-
-STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
-
-
-async def _serve_static(request):
-    """Serve a file from the plugin's static/ directory."""
-    path = request.url_vars.get("path", "")
-    # Prevent path traversal
-    static_real = os.path.realpath(STATIC_DIR)
-    full_path = os.path.realpath(os.path.join(STATIC_DIR, path))
-    if not full_path.startswith(static_real + os.sep):
-        return Response("Forbidden", status=403, content_type="text/plain")
-    if not os.path.isfile(full_path):
-        return Response("Not found", status=404, content_type="text/plain")
-    mime, _ = mimetypes.guess_type(full_path)
-    if mime is None:
-        mime = "application/octet-stream"
-    with open(full_path, "rb") as fh:
-        body = fh.read()
-    return Response(body.decode("utf-8"), status=200, content_type=mime)
 
 
 @hookimpl
